2705ce3e3862b9785f955b7023b11bd796ac8c9a
Andrei Gliga <otzy_007@yahoo.com>
2014-02-27 10:08:52 +0200

    primul controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Resume2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'steps#index'
+  resources :steps
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
