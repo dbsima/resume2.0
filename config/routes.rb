f0d737c9c9c45da7393a3a74f4b615c5593358c3
Andrei Gliga <otzy_007@yahoo.com>
2014-02-27 09:58:26 +0200

    devise instalat

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Resume2::Application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
